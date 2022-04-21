7e53c3700e4aedd13349421637b900fddba6bbf2
Elliot Cubit <elliotcubit@gmail.com>
2022-04-21 15:40:50 -0400

    add ftp decoded

---
+++ decoded/ftp.go
@@ -0,0 +1,8 @@
+type Ftp struct {
+	AuthSslResponse string `json:"auth_ssl_response"`
+	AuthTlsResponse string `json:"auth_tls_response"`
+	Banner          string `json:"banner"`
+	ImplicitTls     bool   `json:"implicit_tls"`
+	StatusCode      int    `json:"status_code"`
+	StatusMeaning   string `json:"status_meaning"`
+}
